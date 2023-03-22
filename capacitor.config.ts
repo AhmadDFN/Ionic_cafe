02faee0e5996fb53ccf63551a8a91f9689b42c05
dasaraul <acmadauliai@example.com>
2023-03-22 23:48:34 +0700

    update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,9 +2,9 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'CafeIK',
+  appName: 'iRzellACafe',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
 };
 
 export default config;
